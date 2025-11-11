d711a7ca2c79e179847fe838b2d41b86b6f83d2c
Hector Rubi <hector.rubi.garcia@outlook.com>
2025-11-10 20:10:36 -0600

    feat: implement AuthService and UserService with corresponding tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,12 @@ import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
 import { AuthController } from './auth/auth.controller';
 import { ConfigModule } from '@nestjs/config';
+import { UserService } from './user/user.service';
+import { AuthService } from './auth/auth.service';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AppController, UsersController, AuthController],
-  providers: [AppService],
+  providers: [AppService, UserService, AuthService],
 })
 export class AppModule {}
