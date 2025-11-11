34468d628a16c8150f5fde88e238048af6538bc6
Hector Rubi <hector.rubi.garcia@outlook.com>
2025-11-10 20:08:59 -0600

    feat: add configuration support with @nestjs/config and create .env.example

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
 import { AuthController } from './auth/auth.controller';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AppController, UsersController, AuthController],
   providers: [AppService],
 })
