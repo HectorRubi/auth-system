94bf5243a37527725ea7831e243b9acd911d9555
Hector Rubi <hector.rubi.garcia@outlook.com>
2025-11-14 12:50:27 -0600

    feat: import config and users module into Auth module.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from 'src/users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { Auth } from './entities/auth.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Auth])],
+  imports: [TypeOrmModule.forFeature([Auth]), ConfigModule, UsersModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
